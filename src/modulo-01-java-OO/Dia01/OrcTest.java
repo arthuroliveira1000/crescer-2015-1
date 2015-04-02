3d5b5887d29faf604b6dba92229d13a4197680e7
Arthur Oliveira da Silva <tuli.oliveira@hotmail.com>
2015-04-02 17:33:10 -0300

    Código de Teste da classe Orc

---
+++ src/modulo-01-java-OO/Dia01/OrcTest.java
@@ -0,0 +1,30 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class OrcTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class OrcTest
+{
+   @Test
+   public void orcNasceCom110Vida() {
+    // Arrange - Montagem dos dados de test
+     Orc orc = new Orc();
+    // Act - execução da ação de testes
+    orc.getVida();
+    // Assert - verificação se o resultado é o esperado
+    int esperado = 110;
+    int resultadoObtido = orc.getVida();
+    
+    assertEquals(resultadoObtido, esperado);
+   
+       
+    }
+}
