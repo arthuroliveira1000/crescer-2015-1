c36f636c27c7c8ad7b452223f6f093467722fe7c
Arthur Oliveira da Silva <tuli.oliveira@hotmail.com>
2015-04-02 16:37:41 -0300

    Exercicio 1.5 - Fazer com que os elfos possam atirar nos orcs, fazendo com que os mesmos percam vidas

---
+++ src/modulo-01-java-OO/Dia01/Orcs.java
@@ -0,0 +1,38 @@
+
+/**
+ * Write a description of class Orcs here.
+ * 
+ * @author Arthur Oliveira 
+ * @version (a version number or a date)
+ */
+public class Orcs
+{
+    // instance variables - replace the example below with your own
+    private static int vida = 110;
+
+    
+    public Orcs() {
+        
+    }
+    
+    public int getVida() {
+            return vida;
+        }   
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+    
+    
+    /**
+     * Constructor for objects of class Orcs
+     
+    public Orcs(int vida)
+    {
+        // initialise instance variables
+       this.
+    }
+    */
+
+    
+}
