dd820f31ed7fbdf8911c8904ec7990607568ed82
Arthur Oliveira da Silva <tuli.oliveira@hotmail.com>
2015-04-06 14:16:44 -0300

    Código Teste da Classe Elfo

--- src/modulo-01-java-OO/Dia01/Elfo.java
+++ src/modulo-01-java-OO/Dia01/Elfo.java
@@ -7,16 +7,14 @@
 public class Elfo
 {
     // Versão correção do tema!!
-    
     private String nome;
     private int flechas, experiencia;
     private int vidaOrc;
-    
-    
-    
+
     // type initializer
     {
         flechas = 42;
+        experiencia = 0;
     }
     
     /**
@@ -25,6 +23,11 @@ public class Elfo
      * @param umNome Nome que o elfo receberá
      * @param flechas Quantidade inicial de flechas
      */
+    
+    Elfo() {
+    
+    }
+    
     public Elfo(String umNome, int flechas)
     {
         //this(umNome);
@@ -39,14 +42,35 @@ public class Elfo
     /**
      * Atira uma flecha e perde uma unidade.
      */
-    public void atirarFlecha(Orc orc) {
+    public void atirarFlecha() {
         //flechas = flechas - 1;
+        Orc orc = new Orc();
         flechas--;
         experiencia++;
         vidaOrc = orc.getVida();
         vidaOrc = vidaOrc - 10;
         orc.setVida(vidaOrc);        
     }
+    
+    public int getFlechas() {
+            return flechas;
+        } 
+        
+        
+    public int getExperiencia() {
+            return experiencia;
+        } 
+        
+        
+   public int getVidaOrc() {
+            return vidaOrc;
+        } 
+        
+       
+    
+    
+    
+    
     
     /* EXERCICIO 1.4
     //public void setNome(String novoNome) {
