4dc26c50e7ecac15188b25ab99ebfe23e234c769
Arthur Oliveira da Silva <tuli.oliveira@hotmail.com>
2015-04-07 17:15:10 -0300

    Criação da classe ItemDoInventario

---
+++ src/modulo-01-java-OO/Dia01/ItemDoInventario.java
@@ -0,0 +1,45 @@
+
+/**
+ * Classe de um item do inventario
+ * 
+ * @author Arthur Oliveira
+ * 
+ */
+public class ItemDoInventario
+{
+    private String descricao;
+    private int quantidade;
+
+        /**
+         * Constructor for objects of class ItemDoInventario com descrição
+         */
+        /**
+         * Constructor for objects of class ItemDoInventario com descrição e quantidade
+         */
+    public ItemDoInventario(String novaDescricao, int novaQuantidade)
+    {
+        this.descricao = novaDescricao;
+        this.quantidade = novaQuantidade;
+    }
+    
+        //GETTERS AND SETTERS
+    public String getDescricao() {
+        return this.descricao;
+    }
+    
+    public int getQuantidade() {
+        return this.quantidade;
+    }
+    
+    /* CRIAR METODOS SET SOMENTE QUANDO PRECISAR!!! MA PRATICA
+     * 
+     public void setDescricao(String novaDescricao) {
+        descricao = novaDescricao;
+    }
+    
+    public void setQuantidade(int novaQuantidade) {
+        quantidade = novaQuantidade;
+    }
+    */
+    
+}
