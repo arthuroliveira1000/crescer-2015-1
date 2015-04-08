219d4ea0d43e42f2445092128a13823b5fed4c44
Arthur Oliveira da Silva <tuli.oliveira@hotmail.com>
2015-04-08 16:32:48 -0300

    Exemplo FOR

---
+++ src/modulo-01-java-OO/Dia01/ContadorDeNumeros.java
@@ -0,0 +1,24 @@
+
+/**
+ * Classe que conta até 3. :v
+ * 
+ * @author Arthur Oliveira 
+ * 
+ */
+public class ContadorDeNumeros
+{
+    public ContadorDeNumeros()
+    {
+       
+    }
+    
+    public void contaAteTres(int limite) {
+    
+        for(int i = 1; i <= limite; i++) {
+            System.out.println(i);
+        }
+        
+        //ctrl + k -> limpa terminal
+        //ctrl + t -> mostra terminal
+    }
+}
