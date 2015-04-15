3dbd48390825e9338bceee9de9f753f33454a5e3
Arthur Oliveira da Silva <tuli.oliveira@hotmail.com>
2015-04-15 17:34:02 -0300

    feat(EstrategiaDeAtaque) modificações no nome da classe EstrategiaDeAtaque

---
+++ src/modulo-01-java-OO/Dia01/EstrategiaDeAtaque.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+/**
+ * Write a description of interface EstrategiaDetaque here.
+ * 
+ * @author Arthur Oliveira 
+ */
+public interface EstrategiaDeAtaque
+{
+   void atacarOrcs(ArrayList<Elfo> elfos, ArrayList<Orc> orcs);
+}
