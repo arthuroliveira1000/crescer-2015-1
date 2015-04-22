8b0953670ee7ea911c4b78c777a97a675c366c93
Arthur Oliveira da Silva <tuli.oliveira@hotmail.com>
2015-04-22 14:57:34 -0300

    Exercicios 17-04

--- Banco de Dados/Exercicios/exercicios_terminar.sql
+++ Banco de Dados/Exercicios/exercicios_terminar.sql
@@ -1,13 +1,36 @@
 --EXERCICIOS 
 
 --1
-select substring(nome, 1, CHARINDEX(' ', nome)) [Primeiro Nome] from associado where idassociado = 1;
+select substring(nome, 1, CHARINDEX(' ', nome) - 1) [Primeiro Nome] from associado;
 
 --2
 select nome , DATEDIFF(yy, DataNascimento , GETDATE()) Idade from associado;
 
 --3
-select nomeempregado, datediff(mm, DataAdmissao, '31/12/2000') Total_De_Meses_trabalhodos_Até_31122000 from empregado where dataadmissao between '01/05/1980' and '20/01/1982';
+select nomeempregado, datediff(mm, DataAdmissao, '31/12/2000') Total_De_Meses_trabalhodos_Até_31122000 from empregado where dataadmissao between convert(DateTime, '01/05/1980', 103) and convert(DateTime, '20/01/1982', 103);
+
+--4
+select top 1 with ties cargo, count(1) [Total de Empregados] from empregado group by cargo order by [Total de Empregados] desc;
 
 --5
-select * from associado group by max(Nome);
+select top 1 with ties nome from associado order by len(nome) desc;
+
+--6
+select nome, DateAdd(year, 50, DataNascimento) as [Ano que Completa 50 Anos], 
+dateName(weekday, DateAdd(year, 50, DataNascimento)) as [Dia da Semana] from associado;
+
+select * from associado;
+
+--7
+select uf, count(nome) as [Quantidade de Cidades por Estado] from cidade group by uf; 
+
+select nome from cidade where uf = 'rs';
+
+--8
+
+
+--9
+select count(IDAssociado) + 1 as [Proximo id que será usado] from Associado;
+
+
+--10
