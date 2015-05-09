9ffd7f17c776f6b606530fe856e0fd9158e59ec5
Arthur Oliveira da Silva <tuli.oliveira@hotmail.com>
2015-05-09 02:07:37 -0300

    Terminar 4 e 5.A

---
+++ módulo III - Programação WEB/Exercícios/exercicios_JAVASCRIPT.js
@@ -0,0 +1,188 @@
+<script>
+
+	//1  
+	
+	var contagemtitulos = [ arsenal, chelsea, liverpool, manchesterunited];
+
+	var arsenal = {
+	  nome: 'Arsenal Football Club',
+	  titulos: [
+		{ desc: 'Nacionais', qtd: 39 },
+		{ desc: 'Continentais', qtd: 0 },
+		{ desc: 'Mundiais', qtd: 0 }
+		]
+	};
+
+	var chelsea = {
+	  nome: 'Chelsea Football Club',
+	  titulos: [
+		{ desc: 'Nacionais', qtd: 5 },
+		{ desc: 'Continentais', qtd: 1 },
+		{ desc: 'Mundiais', qtd: 0 }
+		]
+	};
+	var liverpool  = {
+	  nome: 'Liverpool Football Club',
+	  titulos: [
+		{ desc: 'Nacionais', qtd: 18},
+		{ desc: 'Continentais', qtd: 3 },
+		{ desc: 'Mundiais', qtd: 0 }
+		]
+	};
+
+	var manchesterunited = {
+	  nome: 'Manchester United Football Club',
+	  titulos: [
+		{ desc: 'Nacionais', qtd: 5 },
+		{ desc: 'Continentais', qtd: 3 },
+		{ desc: 'Mundiais', qtd: 57 }
+		]
+	};
+	
+	
+	/* 
+	2.A
+	Crie uma função chamada ordenaPorNacionais(Array) que recebe o array de clubes e retorna o array ordenado pela 
+	quantidade de títulos Nacionais descendente (Z->A).
+	*/
+	function ordenaPorNacionais(array) {
+	
+		return array.sort(
+			function(a, b)
+			{	return a.titulos[0].qtd < b.titulos[0].qtd}
+			);
+	}
+	
+	/* 
+	2.B
+	Crie uma função chamada ordenaPorContinentais(Array) que recebe o array de clubes e retorna o array ordenado 
+	pela quantidade de títulos Continentais descendente (Z->A).
+	*/
+	
+	
+	function ordenaPorContinentais(array) {
+	
+		return array.sort(
+			function(a, b) 
+			{	return a.titulos[1].qtd < b.titulos[1].qtd
+			});
+	
+	}
+	
+	/* 
+	2.C
+	Crie uma função chamada ordenaPorMundiais(Array) que recebe o array de clubes e 
+	retorna o array ordenado pela quantidade de títulos Mundiais descendente (Z->A).
+	*/
+	
+	
+	function ordenaPorMundiais(array) {
+	
+		return array.sort(
+		function(a, b)
+		{ return a.titulos[2].qtd < b.titulos[2].qtd});
+	
+	}
+	
+	
+	
+	
+	/*
+	3.A
+	Crie uma função chamada somarPorNacionais(Array) que recebe o array de clubes e retorna o 
+	somatório de todas quantidades de títulos nacionais entre os clubes do array.
+		*/
+		
+	function somarPorNacionais(array) {
+	
+		var soma = 0;
+		
+		for(var i = 0; i < array.length; i++) {
+			soma+= array[i].titulos[0].qtd; 
+		}
+		 
+		 return soma;
+	}	
+	
+	
+	/*
+	3.B
+	Crie uma função chamada somarPorContinentais(Array) que recebe o array de clubes e retorna o somatório de todas quantidades de títulos continentais entre os clubes do array.
+		*/
+		
+	function somarPorContinentais(array) {
+		
+		var soma = 0;
+		
+		for(var i = 0; i  < array.length; i++) {
+			soma += array[i].titulos[1].qtd;
+		}
+		
+		return soma;
+	
+	}
+	
+	
+	/*
+	3.C
+	Crie uma função chamada somarPorTodosTitulos(Array) que recebe o array de clubes e retorna o somatório de todas quantidades de todos os títulos entre os clubes do array.
+	*/
+
+	function somarPorTodosTitulos(array) {
+	
+		var soma = 0;
+		
+		for(var i = 0; i < array.length; i++) {
+			for(var j = 0; j < array[i].titulos.length; j++) {
+				soma += array[i].titulos[j].qtd;
+			}
+		}
+		
+		return soma;
+	}	
+	
+	/* 
+	4
+	Escreva uma função chamada apenasOsMelhores(Array) que recebe o array de clubes e retorna apenas os clubes que têm mais de 18 títulos ingleses
+	*/
+	
+		function apenasOsMelhores(array) {
+			
+			for(var i = 0; i < array.length; i++) {
+				if(array[i].titulos[0].qtd > 18) 
+					return array[i];
+			}	
+		} 
+		//ARRUMAR
+	
+	
+	/*
+	5.A
+	Escreva uma função chamada apenasOsMelhoresParaJSON(Array) que recebe o array dos clubes que têm mais de 18 títulos ingleses e retorna a representação JSON deles.
+	*/
+	
+	function apenasOsMelhoresParaJSON() {
+	//FAZER
+	
+	
+	}
+	
+	/*
+	5.B
+	Escreva uma função chamada apenasOsMelhoresRecebendoJSON(String) que recebe uma String no formato JSON e retorna o Array de objetos JavaScript.
+	
+	JSON is a format for storing and transporting data.
+
+	JSON is often used when data is sent from a server to a web page.
+
+	JSON - par (chave, valor)
+	*/
+
+	
+	function apenasOsMelhoresRecebendoJSON(array) {
+	
+		return JSON.stringify(array);
+	}
+	
+	
+</script>
