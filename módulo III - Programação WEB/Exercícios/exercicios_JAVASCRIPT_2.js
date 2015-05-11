208c432f5895d87b7aad6166cdc0532c4a5ba444
Arthur Oliveira da Silva <tuli.oliveira@hotmail.com>
2015-05-11 01:29:23 -0300

    Exe. JAVASCRIPT - Parte 2 - começo - fail

---
+++ módulo III - Programação WEB/Exercícios/exercicios_JAVASCRIPT_2.js
@@ -0,0 +1,13 @@
+﻿/*
+1 - Brewdog
+
+Escreva uma função em JS que retorne o número de caracteres diferentes entre dois textos. */
+
+	function brewdog( var1, var2) {
+
+		for(var i  = 0; i < var1.length; i++) {
+			for(var j = 0; j < var2.length;  j ++) {
+				return Math.abs((var1.charAt(i).match(var2.charAt(j))).length);
+			} // -__________________-
+		}
+	}
