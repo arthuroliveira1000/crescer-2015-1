24472aa7201895d265946ea90c4eba5e5713b136
Arthur Oliveira da Silva <tuli.oliveira@hotmail.com>
2015-05-15 17:32:27 -0300

    Exercício - teste - indexOfTest

---
+++ módulo IV - JAVA/apis/src/apis/indexOfTest.java
@@ -0,0 +1,16 @@
+package apis;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class indexOfTest {
+
+	@Test
+	public void teste() {
+		String teste = "abc";
+		assertEquals(1, teste.indexOf("b"));
+
+	}
+
+}
