edff25f5852289246b58049f464b22459c15eb9e
arthuroliveira1000 <tuli.oliveira@hotmail.com>
2015-05-26 00:31:20 -0300

    Organizando home.html e validação de formulário js

--- módulo IV - JAVA/filmator-web/src/main/resources/static/js/javascript.js
+++ módulo IV - JAVA/filmator-web/src/main/resources/static/js/javascript.js
@@ -1,10 +1,8 @@
-
-		function validaFormulario() {
-			var formulario = document.form_cadastro;
-			if(formulario.nome.value == "" || formulario.anoLancamento.value == "" || formulario.sinopse.value =="" || formulario.genero.value=="" || formulario.imagem.value =="") {
-				alert("Para o cadastramento do filme, todos os campos precisam estar preenchidos!");
-				} else {
-					document.form_cadastro.submit();
-					alert(formulario.nome.value + " cadastrado com sucesso!");
-				}
+function validaFormulario() {
+	var formulario = document.form_cadastro;
+		if(formulario.nome.value == "" || formulario.sinopse.value =="" || formulario.genero.value=="" || formulario.anoLancamento.value=="") || formulario.capaDoFilme.value =="" {
+			alert("Preencha todos os campos!");
+			} else {
+				formulario.submit();
 		}
+}
