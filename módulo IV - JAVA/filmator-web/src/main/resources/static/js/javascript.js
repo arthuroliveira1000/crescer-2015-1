5b6cdd411edd405825a799798ab8f1b026926a47
arthuroliveira1000 <tuli.oliveira@hotmail.com>
2015-05-26 20:38:57 -0300

    Validação de formulário de pesquisa

--- módulo IV - JAVA/filmator-web/src/main/resources/static/js/javascript.js
+++ módulo IV - JAVA/filmator-web/src/main/resources/static/js/javascript.js
@@ -1,8 +1,18 @@
 function validaFormulario() {
 	var formulario = document.form_cadastro;
-		if(formulario.nome.value == "" || formulario.sinopse.value =="" || formulario.genero.value=="" || formulario.anoLancamento.value=="") || formulario.capaDoFilme.value =="" {
+		if(formulario.nome.value == "" || formulario.sinopse.value =="" || formulario.genero.value=="" || formulario.anoLancamento.value=="" || formulario.capaDoFilme.value =="") {
 			alert("Preencha todos os campos!");
 			} else {
-				formulario.submit();
+				document.form_cadastro.submit();
+		}
+}
+
+
+function validaFormularioPesquisa() {
+	var formulario = document.form_pesquisa;
+	if(formulario.nome.value == "") {
+		alert("Preencha o campo de pesquisa!");
+		else {
+			formulario.submit();
 		}
 }
