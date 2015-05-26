a365a3ffa50585235073472c6d78a28eefa98e46
arthuroliveira1000 <tuli.oliveira@hotmail.com>
2015-05-26 20:43:36 -0300

    Adicionado método para exibir detalhes de um filme

--- módulo IV - JAVA/filmator-web/src/main/java/filmator/controller/HomeController.java
+++ módulo IV - JAVA/filmator-web/src/main/java/filmator/controller/HomeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import filmator.dao.FilmeDao;
+import filmator.dao.FilmeDAO;
 import filmator.model.Filme;
 import filmator.model.Genero;
 
@@ -19,27 +19,33 @@ import filmator.model.Genero;
 public class HomeController {
 
 	@Inject
-	private FilmeDao filmeDao;
+	private FilmeDAO filmeDao;
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Model model) {
 		model.addAttribute("generos", Genero.values());
 		model.addAttribute("filmes", new ArrayList<>());
-		return "Home";
-	}
-
-	@RequestMapping(value = "/inserir", method = RequestMethod.POST)
-	public String inserir(Model model, Filme filme) {
-		filmeDao.inserir(filme);
 		model.addAttribute("filmes", filmeDao.buscaTodosFilmesJava8());
 		return "Home";
 	}
 
+	@RequestMapping(value = "/inserir", method = RequestMethod.POST)
+	public String inserir(Filme filme) {
+		filmeDao.inserir(filme);
+		return "redirect:/Home"; // volta para a home, limpa o formulário
+	}
+
 	@ResponseBody
 	// @ResponseBody faz transformar o retorno para JSON!
-	@RequestMapping(value = "/buscarTodos", method = RequestMethod.GET)
-	public List<Filme> buscarTodos(Model model) {
-		return filmeDao.buscaTodosFilmesJava8();
+	// @RequestMapping(value = "/buscarTodos", method = RequestMethod.GET)
+	// public List<Filme> buscarTodos(Model model) {
+	// return filmeDao.buscaTodosFilmesJava8();
+	// }
+	@RequestMapping(value = "/buscaFilme", method = RequestMethod.POST)
+	public String buscaDetalhes(String nome, Model model) {
+		model.addAttribute("filmeSelecionado", filmeDao.buscaFilme(nome));
+		return "Home";
+
 	}
 
 }
