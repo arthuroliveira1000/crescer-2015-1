06c1016a551b780c68ce47ed21df1e898ca8e124
arthuroliveira1000 <tuli.oliveira@hotmail.com>
2015-05-26 00:25:03 -0300

    Adicionado Dependency injection

--- módulo IV - JAVA/filmator-web/src/main/java/filmator/dao/FilmeDao.java
+++ módulo IV - JAVA/filmator-web/src/main/java/filmator/dao/FilmeDao.java
@@ -1,19 +1,51 @@
 package filmator.dao;
 
-import java.util.ArrayList;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
 
 import filmator.model.Filme;
 
+@Component
 public class FilmeDao {
 
-	private static List<Filme> filmesSalvos = new ArrayList<Filme>();
+	@Inject
+	private JdbcTemplate jdbcTemplate;
+
+	public void inserir(Filme filme) {
+		jdbcTemplate
+				.update("INSERT INTO Filme (nome, genero, anolancamento, sinopse, capadofilme) VALUES (?, ?, ?, ?, ?)",
+						filme.getNome(), filme.getGenero().toString(),
+						filme.getAnoLancamento(), filme.getSinopse(),
+						filme.getCapaDoFilme());
+	}
 
 	public List<Filme> buscaTodosFilmes() {
-		return filmesSalvos;
+
+		return jdbcTemplate.query("SELECT nome FROM Filme",
+				new RowMapper<Filme>() {
+					@Override
+					public Filme mapRow(ResultSet rs, int rowNum)
+							throws SQLException {
+						Filme filme = new Filme(rs.getString("nome"));
+						return filme;
+					}
+				});
 	}
 
-	public void salvar(Filme filme) {
-		filmesSalvos.add(filme);
+	public List<Filme> buscaTodosFilmesJava8() {
+		return jdbcTemplate.query("SELECT nome FROM Filme", (ResultSet rs,
+				int rowNum) -> {
+			Filme filme = new Filme(rs.getString("nome"));
+			return filme;
+		});
+
 	}
+
 }
