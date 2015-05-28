093d1e210aafb8e520713852cea5935ecf438fe4
arthuroliveira1000 <tuli.oliveira@hotmail.com>
2015-05-28 18:25:26 -0300

    Adicionado método abreHome

--- módulo IV - JAVA/filmator-web/src/main/java/filmator/controller/HomeController.java
+++ módulo IV - JAVA/filmator-web/src/main/java/filmator/controller/HomeController.java
@@ -1,5 +1,7 @@
 package filmator.controller;
 
+import java.util.ArrayList;
+
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
@@ -17,10 +19,17 @@ public class HomeController {
 	@Inject
 	private FilmeDAO filmeDao;
 
+	@RequestMapping(value = "/Home", method = RequestMethod.GET)
+	public String abreHome(Model model) {
+		model.addAttribute("filmes", new ArrayList<>());
+		model.addAttribute("filmes", filmeDao.buscaTodosFilmesJava8());
+		return "Home"; // volta para a home, limpa o formulário
+	}
+
 	@RequestMapping(value = "/inserir", method = RequestMethod.POST)
 	public String inserir(Filme filme) {
 		filmeDao.inserir(filme);
-		return "redirect:/Home"; // volta para a home, limpa o formulário
+		return "redirect:/Home";
 	}
 
 	// @ResponseBody faz transformar o retorno para JSON!
@@ -32,13 +41,11 @@ public class HomeController {
 	@RequestMapping(value = "/buscaFilme", method = RequestMethod.POST)
 	public Filme buscaDetalhes(String nome, Model model) {
 		return filmeDao.buscaFilme(nome);
-
 	}
 
 	@RequestMapping(value = "/excluir", method = RequestMethod.POST)
 	public String excluiFilme(String nome, Model model) {
 		filmeDao.excluir(nome);
 		return "redirect:/Home";
-
 	}
 }
