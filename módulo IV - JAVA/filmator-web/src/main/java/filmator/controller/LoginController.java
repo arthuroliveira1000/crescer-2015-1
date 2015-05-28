a3162150d54da5911e30274b7faa83b05aa32750
arthuroliveira1000 <tuli.oliveira@hotmail.com>
2015-05-28 02:30:28 -0300

    Adicionado controller para tela de login

--- módulo IV - JAVA/filmator-web/src/main/java/filmator/controller/LoginController.java
+++ módulo IV - JAVA/filmator-web/src/main/java/filmator/controller/LoginController.java
@@ -1,16 +1,43 @@
 package filmator.controller;
 
+import java.util.ArrayList;
+
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import filmator.dao.AdministradorDAO;
+import filmator.dao.FilmeDAO;
+import filmator.model.Genero;
+
 @Controller
 public class LoginController {
 
-	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	@Inject
+	private AdministradorDAO adminDao;
+
+	@Inject
+	private FilmeDAO filmeDao;
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String abreTelaLogin(Model model) {
 		return "Login";
+	}
+
+	@RequestMapping(value = "/entrar", method = RequestMethod.POST)
+	public String abreTelaLogin(String login, String senha, Model model) {
+		boolean adminExiste = adminDao.buscaID(login, senha);
+		if (adminExiste) {
+			model.addAttribute("generos", Genero.values());
+			model.addAttribute("filmes", new ArrayList<>());
+			model.addAttribute("filmes", filmeDao.buscaTodosFilmesJava8());
+			return "Home";
+		} else {
+			return "Login";
+		}
 	}
 
 }
